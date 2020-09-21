a6c1cce0bb22d7499b02385c06094e9bd069a62c
irahulcse <1rahulchandra1@gmail.com>
2020-09-21 16:57:34 +0530

    Good First Commit at 2020-09-21-16:57:34

--- lib/home.dart
+++ lib/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
-              height: 50,
+              height: 20,
             ),
             Text(
               "Teaching Machine Learning",
@@ -45,15 +45,69 @@ class _HomeState extends State<Home> {
             Center(
               child: _loading
                   ? Container(
-                    width: 400,
+                      width: 400,
                       child: Column(
                         children: [
                           Image.asset('assets/cat.png'),
-                          SizedBox(height: 50,)
+                          SizedBox(
+                            height: 50,
+                          )
                         ],
                       ),
                     )
                   : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 200,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 17,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        "Take a Photo",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 200,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 17,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        "Camera Roll",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
