4bf79a87bb6dc3ad4d5053c08258229f04e00da2
irahulcse <1rahulchandra1@gmail.com>
2020-09-21 19:22:36 +0530

    hell ow

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cat"s v/s Dogs Classifier',
+      title: 'Flower Classifier',
       home: MySplash(),
       debugShowCheckedModeBanner: false,
     );
